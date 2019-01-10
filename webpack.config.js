6c7c964ad27da46cbfa9270d6bf82f4a6f145670
David Balatero <dbalatero@gmail.com>
2019-01-10 13:43:55 -0800

    [setup] yarn add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const packsDir = path.resolve(__dirname, 'public/packs')
+const packsDir = path.resolve(__dirname, 'public/packs');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: packsDir
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin([packsDir])
+  ]
 };
